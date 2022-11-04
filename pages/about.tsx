22120d43ebc426b37be08d2397e6cb12c18d6e34
misolini <mathiajb@stud.ntnu.no>
2022-11-04 12:12:40 +0100

    Expand 'About' page

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,13 @@ const About = () => {
       <h1>Om quintuplo</h1>
       <p>
         Quintuplo er en nettside som lar deg generere en liste over fem ulike
-        idretter som du kan gjennomføre i løpet av en dag.
+        idretter som du skal gjennomføre i løpet av en dag.
+      </p>
+      <br />
+      <p>
+        I ekte quintuplo-stil skal man ikke generere på ny dersom man er
+        misfornøyd med trekningen. I også god quintuplo-stil skal (minst) fem
+        pils konsumeres på samme kveld som quintuploen gjennomføres.
       </p>
     </div>
   );
